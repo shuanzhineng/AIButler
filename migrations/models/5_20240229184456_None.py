7eb1b865ac0e5b11c12a5a24ae32027b03419ffe
王凡 <wangfan@keanbang.com>
2024-02-29 18:45:10 +0800

    chore: 调整迁移文件

--- migrations/models/5_20240229184456_None.py
+++ migrations/models/5_20240229184456_None.py
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS `user` (
     `modifier_id` BIGINT   COMMENT '修改人id',
     `dept_belong_id` BIGINT   COMMENT '数据归属部门id',
     `name` VARCHAR(255) NOT NULL  COMMENT '用户姓名' DEFAULT '',
-    `username` VARCHAR(255) NOT NULL  COMMENT '用户名',
+    `username` VARCHAR(255) NOT NULL UNIQUE COMMENT '用户名',
     `password` VARCHAR(255) NOT NULL  COMMENT '密码hash值',
     `phone` VARCHAR(255) NOT NULL  COMMENT '手机号' DEFAULT '',
     `email` VARCHAR(255) NOT NULL  COMMENT '邮箱' DEFAULT '',
@@ -24,6 +24,22 @@ CREATE TABLE IF NOT EXISTS `user` (
     `description` VARCHAR(255) NOT NULL  COMMENT '用户描述' DEFAULT '',
     `creator_id` BIGINT
 ) CHARACTER SET utf8mb4 COMMENT='用户表';
+CREATE TABLE IF NOT EXISTS `access_log` (
+    `id` BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '主键id',
+    `create_time` DATETIME(6) NOT NULL  COMMENT '创建时间' DEFAULT CURRENT_TIMESTAMP(6),
+    `update_time` DATETIME(6) NOT NULL  COMMENT '更新时间' DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    `modifier_id` BIGINT   COMMENT '修改人id',
+    `dept_belong_id` BIGINT   COMMENT '数据归属部门id',
+    `api` VARCHAR(255) NOT NULL  COMMENT '接口地址' DEFAULT '',
+    `method` VARCHAR(255) NOT NULL  COMMENT '接口请求方法',
+    `ip_address` VARCHAR(255) NOT NULL  COMMENT 'ip地址' DEFAULT '',
+    `browser` VARCHAR(255) NOT NULL  COMMENT '浏览器信息' DEFAULT '',
+    `os` VARCHAR(255) NOT NULL  COMMENT '操作系统信息' DEFAULT '',
+    `http_status_code` INT NOT NULL  COMMENT 'http状态码' DEFAULT 0,
+    `request_body` LONGTEXT NOT NULL  COMMENT '请求体',
+    `response_body` LONGTEXT NOT NULL  COMMENT '响应体',
+    `creator_id` BIGINT
+) CHARACTER SET utf8mb4 COMMENT='访问日志';
 CREATE TABLE IF NOT EXISTS `dept` (
     `id` BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '主键id',
     `create_time` DATETIME(6) NOT NULL  COMMENT '创建时间' DEFAULT CURRENT_TIMESTAMP(6),
@@ -41,6 +57,20 @@ CREATE TABLE IF NOT EXISTS `dept` (
     `creator_id` BIGINT,
     `parent_id` BIGINT
 ) CHARACTER SET utf8mb4 COMMENT='部门';
+CREATE TABLE IF NOT EXISTS `login_log` (
+    `id` BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '主键id',
+    `create_time` DATETIME(6) NOT NULL  COMMENT '创建时间' DEFAULT CURRENT_TIMESTAMP(6),
+    `update_time` DATETIME(6) NOT NULL  COMMENT '更新时间' DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    `modifier_id` BIGINT   COMMENT '修改人id',
+    `dept_belong_id` BIGINT   COMMENT '数据归属部门id',
+    `username` VARCHAR(255) NOT NULL  COMMENT '用户名',
+    `ip_address` VARCHAR(255) NOT NULL  COMMENT 'ip地址' DEFAULT '',
+    `browser` VARCHAR(255) NOT NULL  COMMENT '浏览器信息' DEFAULT '',
+    `os` VARCHAR(255) NOT NULL  COMMENT '操作系统信息' DEFAULT '',
+    `http_status_code` INT NOT NULL  COMMENT 'http状态码' DEFAULT 0,
+    `is_success` BOOL NOT NULL  COMMENT '是否成功' DEFAULT 0,
+    `creator_id` BIGINT
+) CHARACTER SET utf8mb4 COMMENT='访问日志';
 CREATE TABLE IF NOT EXISTS `menu` (
     `id` BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '主键id',
     `create_time` DATETIME(6) NOT NULL  COMMENT '创建时间' DEFAULT CURRENT_TIMESTAMP(6),
@@ -83,14 +113,14 @@ CREATE TABLE IF NOT EXISTS `role` (
     `data_range` SMALLINT NOT NULL  COMMENT '数据范围' DEFAULT 0,
     `creator_id` BIGINT
 ) CHARACTER SET utf8mb4 COMMENT='角色';
-CREATE TABLE IF NOT EXISTS `user_dept` (
-    `user_id` BIGINT NOT NULL,
-    `dept_id` BIGINT NOT NULL
-) CHARACTER SET utf8mb4 COMMENT='部门';
 CREATE TABLE IF NOT EXISTS `user_role` (
     `user_id` BIGINT NOT NULL,
     `role_id` BIGINT NOT NULL
 ) CHARACTER SET utf8mb4 COMMENT='角色';
+CREATE TABLE IF NOT EXISTS `user_dept` (
+    `user_id` BIGINT NOT NULL,
+    `dept_id` BIGINT NOT NULL
+) CHARACTER SET utf8mb4 COMMENT='部门';
 CREATE TABLE IF NOT EXISTS `role_menu` (
     `role_id` BIGINT NOT NULL,
     `menu_id` BIGINT NOT NULL
