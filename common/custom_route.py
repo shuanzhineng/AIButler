f36cfd630f64ebf348115a2df80e62303791f6b7
王凡 <wangfan@keanbang.com>
2024-03-15 18:39:06 +0800

    fix

--- common/custom_route.py
+++ common/custom_route.py
@@ -20,7 +20,7 @@ NO_LOG_PATHS: list[str] = [
     r"/account/oauth2/token",
     r"/data/label-tasks/\d+?/attachments",
     r"/static/.*",
-    r"/ai-models/train-tasks/\d+?/status",
+    r"/ai-models/train-task-groups/train-tasks/\d+?/status",
     r"/system/celery-workers/online",
     r"/system/celery-workers/offline",
 ]  # 登录接口和涉及文件上传和下载的接口不记录日志
