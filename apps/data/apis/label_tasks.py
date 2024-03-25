f29f81c9242a426bd0644b0781e78db648ab97a9
王凡 <wangfan@keanbang.com>
2024-03-25 10:07:07 +0800

    fix: 修复导出数据集

--- apps/data/apis/label_tasks.py
+++ apps/data/apis/label_tasks.py
@@ -229,8 +229,10 @@ async def export_to_datasets(
     dataset_group_id = items["dataset_group_id"]
     dataset_group_name = items["dataset_group_name"]
     dataset_group_note = items["dataset_group_note"]
-    data = await sample_query_sets.filter(id__in=sample_ids).values(
-        "id", "task_attachment_ids", "annotated_count", "data", "state"
+    data = list(
+        await sample_query_sets.filter(id__in=sample_ids).values(
+            "id", "task_attachment_ids", "annotated_count", "data", "state"
+        )
     )
     xml_result = converter.convert(input_data=data, export_type="xml")
     label_dir = f"static/labelu/{task_id}/labels"
